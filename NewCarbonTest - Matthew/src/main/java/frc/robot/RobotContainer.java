d455161ce6ccc27ea56cca1960511ecbdb63f1e6
Matthew <matthewochoacoc@gmail.com>
2021-11-14 19:10:08 -0800

    Constants, Main, Robot, Robot Container

--- NewCarbonTest - Matthew/src/main/java/frc/robot/RobotContainer.java
+++ NewCarbonTest - Matthew/src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+import frc.robot.constants.Constants;
+import frc.vitruvianlib.utils.JoystickWrapper;
+import frc.vitruvianlib.utils.XBoxTrigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,15 +20,38 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  static final JoystickWrapper leftJoystick = new JoystickWrapper(Constants.leftJoystick);
+  static final JoystickWrapper rightJoystick = new JoystickWrapper(Constants.rightJoystick);
+  static final JoystickWrapper xBoxController = new JoystickWrapper(Constants.xBoxController);
+  
+  static JoystickWrapper testContoller = new JoystickWrapper(4);
+  private static boolean init = false;
+  
+  public final Button[] leftButtons = new Button[2];
+  public final Button[] rightButtons = new Button[2];
+  public final Button[] xBoxButtons = new Button[10];
+  public final Button[] xBoxPOVButtons = new Button[8];
+  
+  public Button xBoxLeftTrigger, xBoxRightTrigger;
+  public Button[] testButtons = new Button[10];
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    initializeSubsystems();
     // Configure the button bindings
     configureButtonBindings();
+  }
+
+  public static boolean getInitializationState() {
+    return init;
+  }
+
+  public static void setInitializationState(boolean state) {
+    init = state;
+  }
+
+  public void initializeSubsystems() {
   }
 
   /**
@@ -34,15 +60,41 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    leftJoystick.invertRawAxis(1, true);
+    rightJoystick.invertRawAxis(0, true);
+    xBoxController.invertRawAxis(1, true);
+    xBoxController.invertRawAxis(5, true);
+    for (int i = 0; i < leftButtons.length; i++)
+      leftButtons[i] = new JoystickButton(leftJoystick, (i + 1));
+    for (int i = 0; i < rightButtons.length; i++)
+      rightButtons[i] = new JoystickButton(rightJoystick, (i + 1));
+    for (int i = 0; i < xBoxButtons.length; i++)
+      xBoxButtons[i] = new JoystickButton(xBoxController, (i + 1));
+    for (int i = 0; i < xBoxPOVButtons.length; i++)
+      xBoxPOVButtons[i] = new POVButton(xBoxController, (i * 45));
+    xBoxLeftTrigger = new XBoxTrigger(xBoxController, 2);
+    xBoxRightTrigger = new XBoxTrigger(xBoxController, 3);
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+  public void disabledInit() {
+    setInitializationState(true);
+  }
+
+  public void robotPeriodic() {
+
+  }
+
+  public void teleOpInit() {
+
+  }
+
+  public void teleOpPeriodic() {
+
   }
 }
